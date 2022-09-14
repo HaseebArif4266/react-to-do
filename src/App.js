977569ff1060f3ed042b58982caab8880dcf72d2
haseeb arif <haseebarif9900@gmail.com>
2022-09-14 00:18:10 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><u>Name: &nbsp;&nbsp;&nbsp;&nbsp; Haseeb Arif</u></h1>
+      <p><b>City: &nbsp;&nbsp;&nbsp;&nbsp; <u>Karachi</u></b></p>
+      <p><b>Contact No: &nbsp;&nbsp;&nbsp;&nbsp; <u>03421234567</u></b></p>
+      <hr></hr>
+      <h1><u>Education Background</u></h1>
+
+                <li><b>School</b></li>
+
+                <li>Methodest Higher Secondary School</li>
+                <li><b>Collage</b></li>
+
+                <li>Superior Science Collage</li>
+
+                <li><b>University</b></li>
+
+                <li>Karachi University</li>
+                <hr></hr>
+                <h1><u>Experience</u></h1>
+                <li>None</li>
+                <hr></hr>
+                <h1><u>Interest/Hobbies</u></h1>
+                <li>Reading</li>
+                <li>Coding</li>
+                <li>Gaming</li>
+                <li>Sports</li>
+
+
+      
+      
+
     </div>
+
   );
-}
+  }
+
+
+
+
+
 
 export default App;
